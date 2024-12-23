ba2e2b4536861e0cbdba4b636b8d2703305a8a16
Pro <msms@i.ua>
2024-12-23 17:52:24 +0200

    )

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,7 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My git app For Users.");
+        System.out.println("The informative application.");
+        // discription
     }
 }
